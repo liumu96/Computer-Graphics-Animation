df275d232914262a2338120e5dda7d861fd3a770
liumu96 <918883717@qq.com>
2023-07-08 21:56:04 +0800

    HeightField: add collision algorithm

--- Fluids/WebGL-Fluids/HeightField/index.js
+++ Fluids/WebGL-Fluids/HeightField/index.js
@@ -19,6 +19,7 @@ let gCamera;
 let gCameraControl;
 let gWaterMaterial;
 let gGrabber;
+let gMouseDown;
 let gPrevTime = 0.0;
 
 // Water Surface
@@ -98,7 +99,59 @@ class WaterSurface {
     this.prevHeights.set(this.bodyHeights);
     this.bodyHeights.fill(0.0);
 
-    for (let i = 0; i < gPhysicsScene.objects.length; i++) {}
+    for (let i = 0; i < gPhysicsScene.objects.length; i++) {
+      let ball = gPhysicsScene.objects[i];
+      let pos = ball.pos;
+      let br = ball.radius;
+      let h2 = this.spacing * this.spacing;
+
+      let x0 = Math.max(0, cx + Math.floor((pos.x - br) * h1));
+      let x1 = Math.min(this.numX - 1, cx + Math.floor((pos.x + br) * h1));
+      let z0 = Math.max(0, cz + Math.floor((pos.z - br) * h1));
+      let z1 = Math.min(this.numZ - 1, cz + Math.floor((pos.z + br) * h1));
+
+      for (let xi = x0; xi <= x1; xi++) {
+        for (let zi = z0; zi < z1; zi++) {
+          let x = (xi - cx) * this.spacing;
+          let z = (zi - cz) * this.spacing;
+          let r2 = (pos.x - x) * (pos.x - x) + (pos.z - z) * (pos.z - z);
+          if (r2 < br * br) {
+            let bodyHalfHeight = Math.sqrt(br * br - r2);
+            let waterHeight = this.heights[xi * this.numZ + zi];
+
+            let bodyMin = Math.max(pos.y - bodyHalfHeight, 0.0);
+            let bodyMax = Math.min(pos.y + bodyHalfHeight, waterHeight);
+            var bodyHeight = Math.max(bodyMax - bodyMin, 0.0);
+            if (bodyHeight > 0.0) {
+              ball.applyForce(-bodyHeight * h2 * gPhysicsScene.gravity.y);
+              this.bodyHeights[xi * this.numZ + zi] += bodyHeight;
+            }
+          }
+        }
+      }
+    }
+    for (let iter = 0; iter < 2; iter++) {
+      for (let xi = 0; xi < this.numX; xi++) {
+        for (let zi = 0; zi < this.numZ; zi++) {
+          let id = xi * this.numZ + zi;
+
+          let num = xi > 0 && xi < this.numX - 1 ? 2 : 1;
+          num += zi > 0 && zi < this.numZ - 1 ? 2 : 1;
+          let avg = 0.0;
+          if (xi > 0) avg += this.bodyHeights[id - this.numZ];
+          if (xi < this.numX - 1) avg += this.bodyHeights[id + this.numZ];
+          if (zi > 0) avg += this.bodyHeights[id - 1];
+          if (zi < this.numZ - 1) avg += this.bodyHeights[id + 1];
+          avg /= num;
+          this.bodyHeights[id] = avg;
+        }
+      }
+    }
+
+    for (let i = 0; i < this.numCells; i++) {
+      let bodyChange = this.bodyHeights[i] - this.prevHeights[i];
+      this.heights[i] += this.alpha * bodyChange;
+    }
   }
 
   simulateSurface() {
@@ -118,7 +171,7 @@ class WaterSurface {
         sumH += i > 0 ? this.heights[id - this.numZ] : h;
         sumH += i < this.numX - 1 ? this.heights[id + this.numZ] : h;
         sumH += j > 0 ? this.heights[id - 1] : h;
-        sumH += j < this.numZ ? this.heights[id + 1] : h;
+        sumH += j < this.numZ - 1 ? this.heights[id + 1] : h;
         this.velocities[id] += gPhysicsScene.dt * c * (sumH - 4.0 * h);
         this.heights[id] += (0.25 * sumH - h) * pd; // positional damping
       }
@@ -172,17 +225,94 @@ class Ball {
     gThreeScene.add(this.visMesh);
   }
 
-  handleCollision(other) {}
+  handleCollision(other) {
+    let dir = new THREE.Vector3();
+    dir.subVectors(other.pos, this.pos);
+    let d = dir.length();
 
-  simulate() {}
+    let minDist = this.radius + other.radius;
+    if (d >= minDist) return;
 
-  applyforce(force) {}
+    dir.multiplyScalar(1.0 / d);
+    let corr = (minDist - d) / 2.0;
+    this.pos.addScaledVector(dir, -corr);
+    other.pos.addScaledVector(dir, corr);
 
-  startGrab(pos) {}
+    let v1 = this.vel.dot(dir);
+    let v2 = other.vel.dot(dir);
 
-  moveGrab(pos, vel) {}
+    let m1 = this.mass;
+    let m2 = other.mass;
 
-  endGrab(pos, vel) {}
+    let newV1 =
+      (m1 * v1 + m2 * v2 - m2 * (v1 - v2) * this.restitution) / (m1 + m2);
+    let newV2 =
+      (m1 * v1 + m2 * v2 - m1 * (v2 - v1) * this.restitution) / (m1 + m2);
+
+    this.vel.addScaledVector(dir, newV1 - v1);
+    other.vel.addScaledVector(dir, newV2 - v2);
+  }
+
+  simulate() {
+    if (this.grabbed) return;
+
+    this.vel.addScaledVector(gPhysicsScene.gravity, gPhysicsScene.dt);
+    this.pos.addScaledVector(this.vel, gPhysicsScene.dt);
+
+    let wx =
+      0.5 * gPhysicsScene.tankSize.x -
+      this.radius -
+      0.5 * gPhysicsScene.tankBorder;
+    let wz =
+      0.5 * gPhysicsScene.tankSize.z -
+      this.radius -
+      0.5 * gPhysicsScene.tankBorder;
+
+    if (this.pos.x < -wx) {
+      this.pos.x = -wx;
+      this.vel.x = -this.restitution * this.vel.x;
+    }
+    if (this.pos.x > wx) {
+      this.pos.x = wx;
+      this.vel.x = -this.restitution * this.vel.x;
+    }
+    if (this.pos.z < -wz) {
+      this.pos.z = -wz;
+      this.vel.z = -this.restitution * this.vel.z;
+    }
+    if (this.pos.z > wz) {
+      this.pos.z = wz;
+      this.vel.z = -this.restitution * this.vel.z;
+    }
+    if (this.pos.y < this.radius) {
+      this.pos.y = this.radius;
+      this.vel.y = -this.restitution * this.vel.y;
+    }
+
+    this.visMesh.position.copy(this.pos);
+    this.visMesh.geometry.computeBoundingSphere();
+  }
+
+  applyForce(force) {
+    this.vel.y += (gPhysicsScene.dt * force) / this.mass;
+    this.vel.multiplyScalar(0.999);
+  }
+
+  startGrab(pos) {
+    this.grabbed = true;
+    this.pos.copy(pos);
+    this.visMesh.position.copy(pos);
+  }
+
+  moveGrabbed(pos, vel) {
+    this.pos.copy(pos);
+    this.visMesh.position.copy(pos);
+  }
+
+  endGrab(pos, vel) {
+    this.grabbed = false;
+    this.vel.copy(vel);
+  }
 }
 
 function initScene(scene) {
@@ -238,7 +368,13 @@ function simulate() {
 
   gPhysicsScene.waterSurface.simulate();
 
-  for (let i = 0; i < gPhysicsScene.objects.length; i++) {}
+  for (let i = 0; i < gPhysicsScene.objects.length; i++) {
+    obj = gPhysicsScene.objects[i];
+    obj.simulate();
+    for (let j = 0; j < i; j++) {
+      obj.handleCollision(gPhysicsScene.objects[j]);
+    }
+  }
 }
 
 function render() {
@@ -347,23 +483,102 @@ function initThreeScene() {
   gCameraControl.target.set(0.0, 0.8, 0.0);
 
   // Grabber
+
+  gGrabber = new Grabber();
+  container.addEventListener("pointerdown", onPointer, false);
+  container.addEventListener("pointermove", onPointer, false);
+  container.addEventListener("pointerup", onPointer, false);
 }
 
 class Grabber {
-  constructor() {}
+  constructor() {
+    this.raycaster = new THREE.Raycaster();
+    this.raycaster.layers.set(1);
+    this.raycaster.params.Line.threshold = 0.1;
+    this.physicsObject = null;
+    this.distance = 0.0;
+    this.prevPos = new THREE.Vector3();
+    this.vel = new THREE.Vector3();
+    this.time = 0.0;
+  }
 
-  increaseTime(dt) {}
+  increaseTime(dt) {
+    this.time += dt;
+  }
 
-  updateRaycaster(x, y) {}
+  updateRaycaster(x, y) {
+    let rect = gRenderer.domElement.getBoundingClientRect();
+    this.mousePos = new THREE.Vector2();
+    this.mousePos.x = ((x - rect.left) / rect.width) * 2 - 1;
+    this.mousePos.y = -((y - rect.top) / rect.height) * 2 + 1;
+    this.raycaster.setFromCamera(this.mousePos, gCamera);
+  }
 
-  start(x, y) {}
+  start(x, y) {
+    this.physicsObject = null;
+    this.updateRaycaster(x, y);
+    let intersects = this.raycaster.intersectObjects(gThreeScene.children);
+    if (intersects.length > 0) {
+      var obj = intersects[0].object.userData;
+      if (obj) {
+        this.physicsObject = obj;
+        this.distance = intersects[0].distance;
+        let pos = this.raycaster.ray.origin.clone();
+        pos.addScaledVector(this.raycaster.ray.direction, this.distance);
+        this.physicsObject.startGrab(pos);
+        this.prevPos.copy(pos);
+        this.vel.set(0.0, 0.0, 0.0);
+        this.time = 0.0;
+        if (gPhysicsScene.paused) run();
+      }
+    }
+  }
 
-  move(x, y) {}
+  move(x, y) {
+    if (this.physicsObject) {
+      this.updateRaycaster(x, y);
+      let pos = this.raycaster.ray.origin.clone();
+      pos.addScaledVector(this.raycaster.ray.direction, this.distance);
 
-  end(x, y) {}
+      this.vel.copy(pos);
+      this.vel.sub(this.prevPos);
+      if (this.time > 0.0) this.vel.divideScalar(this.time);
+      else this.vel.set(0.0, 0.0, 0.0);
+      this.prevPos.copy(pos);
+      this.time = 0.0;
+
+      this.physicsObject.moveGrabbed(pos, this.vel);
+    }
+  }
+
+  end(x, y) {
+    if (this.physicsObject) {
+      this.physicsObject.endGrab(this.prevPos, this.vel);
+      this.physicsObject = null;
+    }
+  }
 }
 
-function onPointer(evt) {}
+function onPointer(evt) {
+  evt.preventDefault();
+  if (evt.type == "pointerdown") {
+    gGrabber.start(evt.clientX, evt.clientY);
+    gMouseDown = true;
+    if (gGrabber.physicsObject) {
+      gCameraControl.saveState();
+      gCameraControl.enabled = false;
+    }
+  } else if (evt.type == "pointermove" && gMouseDown) {
+    gGrabber.move(evt.clientX, evt.clientY);
+  } else if (evt.type == "pointerup") {
+    if (gGrabber.physicsObject) {
+      gGrabber.end();
+      gCameraControl.reset();
+    }
+    gMouseDown = false;
+    gCameraControl.enabled = true;
+  }
+}
 
 function onWindowResize() {
   gCamera.aspect = window.innerWidth / window.innerHeight;
@@ -372,14 +587,23 @@ function onWindowResize() {
   gRenderTarget.setSize(window.innerWidth, window.innerHeight);
 }
 
-function run() {}
+function run() {
+  let button = document.getElementById("buttonRun");
+  if (gPhysicsScene.paused) button.innerHTML = "Stop";
+  else button.innerHTML = "Run";
+  gPhysicsScene.paused = !gPhysicsScene.paused;
+}
 
-function restart() {}
+function restart() {
+  location.reload();
+}
 
 function update() {
   let time = performance.now();
   let dt = (time - gPrevTime) / 1000.0;
   gPrevTime = time;
+
+  gPhysicsScene.dt = Math.min(1.0 / 30.0, 2.0 * dt);
 
   simulate();
   render();
